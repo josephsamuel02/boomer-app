5675dbea7f31572b605437e93c4a47368f7d0b73
Joseph Samuel <mrsamd02@gmail.com>
2025-07-20 01:36:12 +0100

    night

---
+++ src/pages/Admin/Dashboard.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="w-full h-auto">
+      <h3>okay</h3>
+    </div>
+  );
+};
+
+export default Dashboard;
--- src/pages/MoviePage/Banner.tsx
+++ src/pages/MoviePage/Banner.tsx
@@ -88,7 +88,7 @@ const Banner = ({ movieData }: compData) => {
               <iframe
                 src={`https://www.youtube.com/embed/${youtubVid}?si=yj-GZA3legF6RuAt`}
                 title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerPolicy="strict-origin-when-cross-origin"
                 allowFullScreen
                 className="w-full h-full object-contain"
