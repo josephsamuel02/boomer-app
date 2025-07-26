eb6d24dffef115846f4c4ed30fb03d4e04f115ea
Joseph Samuel <mrsamd02@gmail.com>
2025-07-26 07:00:35 +0100

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,42 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Your App Name",
+        short_name: "AppName",
+        description: "Your app description",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      // workbox: {
+      //   // defining cached files formats
+      //   globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      // },
+    }),
+  ],
   build: {
     outDir: "dist", // Ensure this is where your build output goes
   },
